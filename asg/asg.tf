00c801e51fda6699ede470b5c368af466c079731
jiro1 <feratimrak1@gmail.com>
2021-03-19 13:33:42 -0500

    removed key

--- asg/asg.tf
+++ asg/asg.tf
@@ -6,8 +6,8 @@ module "asg-elb" {
   desired_capacity = var.config["desired_capacity"]
   max_size         = var.config["max_size"]
   min_size         = var.config["min_size"]
-  key_name         = var.config["key_name"]
-  key_location     = var.config["key_location"]
+  //key_name         = var.config["key_name"]
+  //key_location     = var.config["key_location"]
   ssh_cidr_blocks  = [
     "127.0.0.1/32",
     "0.0.0.0/0"
--- asg/envs/dev.tfvars
+++ asg/envs/dev.tfvars
@@ -4,6 +4,6 @@ config = {
   desired_capacity = 3
   max_size         = 99
   min_size         = 2
-  key_name         = "wordpress_key"
-  key_location     = "~/.ssh/id_rsa.pub" 
+  //key_name         = "wordpress_key"
+  //key_location     = "~/.ssh/id_rsa.pub" 
 }
